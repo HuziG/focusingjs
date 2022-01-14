9fbaac80b84d1d96045ed3d20ce530f9a5f599ba
zhangxiaoming； <1067408814@qq.com>
2022-01-14 16:31:02 +0800

    添加修改背景色代码

--- focusingjs.js
+++ focusingjs.js
@@ -5,15 +5,15 @@ class FocusingJs {
     this.id = id
 
     this.styleObj = {
-      bgc: '', // - 背景色
+      backgroundColor: '', // - 背景色
       lineHight: '', // - 行高
       fontSize: '', // - 字体大小
-      fontColor: '', // 字体颜色
+      color: '', // 字体颜色
       letterSpacing: '', // - 字间距
-      containerWidth: '' // - 版幅宽度
+      width: '' // - 版幅宽度
     }
 
-    this.init()
+    // this.init()
   }
 
   init () {
@@ -26,6 +26,7 @@ class FocusingJs {
       htmlDom.appendChild(newDiv)
     }
 
+    this.focusingJs = document.querySelectorAll('focusing-js')[0]
     this.focusingJsContainer = document.querySelectorAll('focusing-js .main-container')[0]
   }
 
@@ -79,12 +80,12 @@ class FocusingJs {
 
       try {
         returnValue = {
-          bgc: value.bgc,
+          backgroundColor: value.backgroundColor,
           lineHight: value.lineHight,
           fontSize: value.fontSize,
-          fontColor: value.fontColor,
+          color: value.color,
           letterSpacing: value.letterSpacing,
-          containerWidth: value.containerWidth
+          width: value.width
         }
       } catch (e) {
         // 清除本地存储
@@ -97,17 +98,46 @@ class FocusingJs {
   }
 
   /**
-   * 设置默认样式
-   * @returns {{lineHight: string, letterSpacing: string, fontSize: string, fontColor: string, bgc: string, containerWidth: string}}
+   * 获取默认样式
+   * @returns {{backgroundColor: string, color: string, lineHight: string, width: string, letterSpacing: string, fontSize: string}}
    */
   getDefaultStyle () {
     return {
-      bgc: '#eeeeee',
+      backgroundColor: '#eeeeee',
       lineHight: '8rem',
       fontSize: '3rem',
-      fontColor: '#333333',
+      color: '#333333',
       letterSpacing: '0.5rem',
-      containerWidth: '0 30%'
+      width: '0 30%'
     }
   }
+
+  /**
+   * 修改样式
+   * @param styleKey 修改样式
+   * @param value 修改值
+   */
+  static changeStyle (styleKey, value) {
+    this.focusingJs = document.querySelectorAll('focusing-js')[0]
+    this.focusingJs.style[styleKey] = value
+    this.styleObj[styleKey] = value
+  }
+}
+
+/**
+ * 修改阅读背景色
+ * @param el 当前元素
+ * @param bc 背景色
+ * @param tc 文字颜色
+ */
+function changeBc(el, bc, tc) {
+  FocusingJs.changeStyle('backgroundColor', bc)
+  FocusingJs.changeStyle('color', tc)
+
+  const blocks = document.querySelectorAll('focusing-js .main-color-container .block')
+  blocks.forEach(item => {
+    item.innerHTML = 'Aa'
+  })
+
+  el.innerHTML = `<span style="color: #17A34A">-</span>`
 }
